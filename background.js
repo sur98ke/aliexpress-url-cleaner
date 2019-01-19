96d89ae870718b1805a038b97a1967541d857629
Artur Mansurov <sur98ke@gmail.com>
2019-01-20 01:46:18 +0600

    Add feature to move from store page to global product page Add ability to work on pages in local languages

--- background.js
+++ background.js
@@ -1,16 +1,33 @@
 function removeQueryParams(requestDetails) {
-  const url = new URL(requestDetails.url);
-  if(url.search.length === 0) return {};
-  const trimmed = url.origin + url.pathname;
+    console.log("removeQueryParams");
+  const url = requestDetails.url;
+
+  const re_good = new RegExp(
+    "^\\w+://\\w+\\.aliexpress\\.com/item/[^/?]+/\\d+\\.html$"
+    );
+  if (url.match(re_good)){
+    return {};
+    }
+
+  const re_bad = new RegExp(
+    "^(\\w+://\\w+\\.aliexpress\\.com)/.+[/_](\\d+)\\.html(?:\\?[^/]+)?$"
+    );
+  const res = re_bad.exec(url);
+  if (res === null) {
+    return {};
+    }
+  const protohost = res[1];
+  const productID = res[2];
+  const cleanurl = protohost + "/item/_/" + productID + ".html";
   return {
-    redirectUrl: trimmed
+    redirectUrl: cleanurl
   };
 }
 
 browser.webRequest.onBeforeRequest.addListener(
   removeQueryParams,
   {
-    urls: ["*://www.aliexpress.com/item/*", "*://www.aliexpress.com/store/product/*"]
+    urls: ["*://*.aliexpress.com/item/*", "*://*.aliexpress.com/store/product/*"]
   },
   ["blocking"]
 );
